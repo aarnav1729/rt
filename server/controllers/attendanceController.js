2088ed2ec1620c042eaea203222778c3e8cf49ff
Aarnav Singh <aarnavsingh@Aarnavs-MacBook-Air.local>
2024-07-09 13:06:38 +0530

    updated

--- server/controllers/attendanceController.js
+++ server/controllers/attendanceController.js
@@ -22,15 +22,15 @@ const getMembers = async (req, res) => {
 
 const calculateDistance = (lat1, lon1, lat2, lon2) => {
   const R = 6371e3; // metres
-  const φ1 = lat1 * Math.PI/180; // φ, λ in radians
-  const φ2 = lat2 * Math.PI/180;
-  const Δφ = (lat2-lat1) * Math.PI/180;
-  const Δλ = (lon2-lon1) * Math.PI/180;
+  const φ1 = lat1 * Math.PI / 180; // φ, λ in radians
+  const φ2 = lat2 * Math.PI / 180;
+  const Δφ = (lat2 - lat1) * Math.PI / 180;
+  const Δλ = (lon2 - lon1) * Math.PI / 180;
 
-  const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-            Math.cos(φ1) * Math.cos(φ2) *
-            Math.sin(Δλ/2) * Math.sin(Δλ/2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) *
+    Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
   const d = R * c; // in metres
   return d;
@@ -47,8 +47,8 @@ const markAttendance = async (req, res) => {
 
     // Validate the user's location
     const distance = calculateDistance(event.latitude, event.longitude, latitude, longitude);
-    const oneMileInMeters = 1609.34;
-    if (distance > oneMileInMeters) {
+    const twoMilesInMeters = 3218.69; // 2 miles in meters
+    if (distance > twoMilesInMeters) {
       return res.status(400).json({ message: 'You are not within the allowed range to mark attendance' });
     }
 
