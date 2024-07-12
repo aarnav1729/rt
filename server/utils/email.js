9616db188f1151353715e88d4a624e17a8e10351
Aarnav Singh <aarnavsingh@Aarnavs-MacBook-Air.local>
2024-07-12 13:05:52 +0530

    updates

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,17 +1,49 @@
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: "rotaryclubmoinabad@gmail.com",
-    pass: 'gjur imcc rnnb jcf',
-  },
-});
+const OAuth2 = google.auth.OAuth2;
 
-const sendEmail = (recipient, subject, text) => {
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+  });
+
+  const accessToken = await new Promise((resolve, reject) => {
+    oauth2Client.getAccessToken((err, token) => {
+      if (err) {
+        reject("Failed to create access token :(");
+      }
+      resolve(token);
+    });
+  });
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.EMAIL_USER,
+      accessToken,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    },
+  });
+
+  return transporter;
+};
+
+const sendEmail = async (recipient, subject, text) => {
+  const emailTransporter = await createTransporter();
+  
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: recipient,
@@ -19,7 +51,7 @@ const sendEmail = (recipient, subject, text) => {
     text,
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
+  emailTransporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       return console.log('Error while sending email:', error);
     }
