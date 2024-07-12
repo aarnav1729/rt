cf46ca44313998077e8a6159070a7d24ea94f872
Aarnav Singh <aarnavsingh@Aarnavs-MacBook-Air.local>
2024-07-12 14:02:57 +0530

    updates

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,62 +1,70 @@
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const OAuth2 = google.auth.OAuth2;
 
 const createTransporter = async () => {
   const oauth2Client = new OAuth2(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
+    '960465863169-99bl4c3t83eqdjim6tdvshr2b2huij06.apps.googleusercontent.com',
+    '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',
+    'https://developers.google.com/oauthplayground'
   );
 
   oauth2Client.setCredentials({
-    refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+    refresh_token: '1//04X91d0TPMs-JCgYIARAAGAQSNwF-L9IroWvIAJS0SRZrraYLlbiTxS2z6vQUXjsDHmDCTW85P3VsdFAP0CcvfKzZh7PebZ7JFcI',
   });
 
-  const accessToken = await new Promise((resolve, reject) => {
-    oauth2Client.getAccessToken((err, token) => {
-      if (err) {
-        reject("Failed to create access token :(");
-      }
-      resolve(token);
+  try {
+    const accessToken = await new Promise((resolve, reject) => {
+      oauth2Client.getAccessToken((err, token) => {
+        if (err) {
+          console.error('Failed to create access token:', err);
+          reject("Failed to create access token :(");
+        }
+        resolve(token);
+      });
     });
-  });
 
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: process.env.EMAIL_USER,
-      accessToken,
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-    },
-  });
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: 'aarnavsingh836@gmail.com',
+        accessToken,
+        clientId: '960465863169-99bl4c3t83eqdjim6tdvshr2b2huij06.apps.googleusercontent.com',
+        clientSecret: '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',
+        refreshToken: '1//04X91d0TPMs-JCgYIARAAGAQSNwF-L9IroWvIAJS0SRZrraYLlbiTxS2z6vQUXjsDHmDCTW85P3VsdFAP0CcvfKzZh7PebZ7JFcI',
+      },
+    });
 
-  return transporter;
+    return transporter;
+  } catch (error) {
+    console.error('Error creating transporter:', error);
+    throw new Error('Error creating transporter');
+  }
 };
 
 const sendEmail = async (recipient, subject, text) => {
-  const emailTransporter = await createTransporter();
-  
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: recipient,
-    subject,
-    text,
-  };
+  try {
+    const emailTransporter = await createTransporter();
+    
+    const mailOptions = {
+      from: 'aarnavsingh836@gmail.com',
+      to: recipient,
+      subject,
+      text,
+    };
 
-  emailTransporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log('Error while sending email:', error);
-    }
-    console.log('Email sent: ' + info.response);
-  });
+    emailTransporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error while sending email:', error);
+        return;
+      }
+      console.log('Email sent:', info.response);
+    });
+  } catch (error) {
+    console.error('Error in sendEmail:', error);
+  }
 };
 
 module.exports = { sendEmail };
